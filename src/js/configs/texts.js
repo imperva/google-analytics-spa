5ece13eb4534029217ddfe879823aca5beef0723
michael.rodov <michael.rodov@imperva.com>
2020-03-19 16:16:53 +0200

    - removed internal information

--- src/js/configs/texts.js
+++ src/js/configs/texts.js
@@ -1,6 +1,2 @@
 export default {
-    notifications: {
-        getDashboardDataFailedNotificationText: () => 'Failed to fetch dashboard data',
-        getSsessionTimeOutText: () => 'Session timed out, please re-login.',
-    },
 };
