77fb5d6f2749015b4b9182326999025d99c0e0f7
michael.rodov <michael.rodov@imperva.com>
2020-05-23 17:06:12 +0300

    - testing exceptions sending

---
+++ src/js/components/google-analytics/GoogleAnalytics.exceptions.test.js
@@ -0,0 +1,34 @@
+import {tracker} from './GoogleAnalytics';
+
+function getExceptionReportObject(exDescription, exFatal) {
+    return {
+        hitType: 'exception',
+        exDescription: exDescription,
+        exFatal: exFatal,
+        transport: 'beacon'
+    };
+}
+
+describe('reporting exception', function () {
+    let _tracker = tracker();
+    const gaSpy = jest.spyOn(global, 'ga');
+    const ERROR_DESC = 'my error';
+    const IF_FATAL = false;
+
+    function validateReportEventResult(exDescription, exFatal) {
+        expect(gaSpy).toHaveBeenCalledTimes(2);
+        expect(gaSpy.mock.calls[1][0]).toEqual('tracker.send');
+        expect(gaSpy.mock.calls[1][1]).toEqual(getExceptionReportObject(exDescription, exFatal));
+    }
+
+    afterEach(() => {
+        gaSpy.mockReset();
+    });
+
+    it('should validate that ga function is called when reporting an exception', function () {
+        _tracker.reportException(ERROR_DESC, IF_FATAL);
+        validateReportEventResult(ERROR_DESC, IF_FATAL);
+    });
+
+
+});
